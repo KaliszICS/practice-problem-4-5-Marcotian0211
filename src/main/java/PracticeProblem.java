ae0d54d7671290160738cb1feab49cdd441bee18
Marcotian0211 <350327743@tdsb.ca>
2026-04-23 15:19:32 +0000

    update practice problem

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,56 @@ public class PracticeProblem {
 
 	}
 
-	
+public static String triangle(int num) {
+    String word = "";
+    
+    for (int i = 1; i <=num; i++) {
+        for (int j = 1; j <= i; j++) {
+            word += "*";
+        }
+        if (i <= num) {
+            word += "\n";
+        }
+    }
+    
+    return word;
+}
+public static String pyramid(int n) {
+    String result = "";
+    
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n - i; j++) {
+            result += " ";
+        }
+        
+        for (int k = 1; k <= i; k++) {
+            result += "*";
+            if (k < i) {
+                result += " ";
+            }
+        }
+        if (i <= n) {
+            result += "\n";
+        }
+    }
+    
+    return result;
+}
+public static String multiplicationTable(int num){
+String word="";
+ for(int i=1;i<=num;i++){
+    for(int j=1;j<=num;j++){
+       word+=(j*i);
+    if(j<num){
+        word+=(" ");
+    }
+    
+ }
+ if(i<=num){
+    word+="\n";
+ }
 
 }
+return word;
+
+}}
